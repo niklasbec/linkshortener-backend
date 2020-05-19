61df4860565824e85f3323da90d0fdb919a95dc9
Niklas Becker <48069565+niklasbec@users.noreply.github.com>
2020-05-19 13:21:02 +0200

    x

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const cors = require("cors")
 require("dotenv/config")
 
 
 app.use(express.json());
+app.use(cors())
 
 //Import routes
 const urlRouter = require("./routers/url")
